ca4f60c7be30c0b5985c0c25ae0435e07a9e9fb1
amyjoscelyn <amy.joscelyn@flatironschool.com>
2016-10-31 11:38:23 -0400

    GameScene: zombie sprite added to represent player

--- ZombieConga/ZombieConga/GameScene.swift
+++ ZombieConga/ZombieConga/GameScene.swift
@@ -10,13 +10,31 @@ import SpriteKit
 
 class GameScene: SKScene
 {
+    let zombie1 = SKSpriteNode(imageNamed: "zombie1")
+    
     //SpriteKit calls this method before it presents this scene in a view, so it's a good place to do some initial setup of the scene's contents
     override func didMove(to view: SKView)
     {
         backgroundColor = SKColor.black
         
         let background = SKSpriteNode(imageNamed: "background1")
+        /* This does the same as pinning the center to the center of the scene, because this is pinning the bottom left of the background to the bottom left of the scene!
+        background.anchorPoint = CGPoint.zero
+        background.position = CGPoint.zero
+         */
+        background.anchorPoint = CGPoint(x: 0.5, y: 0.5) // default
         background.position = CGPoint(x: size.width/2, y: size.height/2)
+        background.zPosition = -1
+        
+//        background.zRotation = CGFloat(M_PI) / 8
+        
         addChild(background)
+        
+        zombie1.position = CGPoint(x: 400, y: 400)
+        
+        addChild(zombie1)
+        
+        let mySize = background.size
+        print("Size: \(mySize)")
     }
 }
