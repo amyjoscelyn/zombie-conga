354bbf05faef0b105cf13a3237c50876208c5baf
amyjoscelyn <amy.joscelyn@flatironschool.com>
2016-11-08 11:00:18 -0500

    Added custom font file for labels to use.  Labels show player lives and cats collected

--- ZombieConga/ZombieConga/GameScene.swift
+++ ZombieConga/ZombieConga/GameScene.swift
@@ -28,6 +28,9 @@ class GameScene: SKScene
     var gameOver = false
     let cameraNode = SKCameraNode()
     let cameraMovePointsPerSecond: CGFloat = 200.0
+    let livesLabel = SKLabelNode(fontNamed: "Glimstick")
+    let catsLabel = SKLabelNode(fontNamed: "Glimstick")
+    
     var cameraRect: CGRect {
         let x = cameraNode.position.x - size.width/2 + (size.width - playableRect.width) / 2
         let y = cameraNode.position.y - size.height/2 + (size.height - playableRect.height) / 2
@@ -145,6 +148,28 @@ class GameScene: SKScene
         addChild(cameraNode)
         camera = cameraNode
         cameraNode.position = CGPoint(x: size.width / 2, y: size.height / 2)
+        
+        livesLabel.text = "Lives: X"
+        livesLabel.fontColor = SKColor.black
+        livesLabel.fontSize = 100
+        livesLabel.zPosition = 150
+        livesLabel.horizontalAlignmentMode = .left
+        livesLabel.verticalAlignmentMode = .bottom
+        livesLabel.position = CGPoint(
+            x: -playableRect.size.width / 2 + CGFloat(20),
+            y: -playableRect.size.height / 2 + CGFloat(20))
+        cameraNode.addChild(livesLabel)
+        
+        catsLabel.text = "Cats: X"
+        catsLabel.fontColor = SKColor.black
+        catsLabel.fontSize = 100
+        catsLabel.zPosition = 150
+        catsLabel.horizontalAlignmentMode = .right
+        catsLabel.verticalAlignmentMode = .bottom
+        catsLabel.position = CGPoint(
+            x: playableRect.size.width / 2 - CGFloat(20),
+            y: -playableRect.size.height / 2 + CGFloat(20))
+        cameraNode.addChild(catsLabel)
     }
     
     override func update(_ currentTime: TimeInterval)
@@ -193,6 +218,8 @@ class GameScene: SKScene
         boundsCheckZombie()
         moveTrain()
         moveCamera()
+        
+        livesLabel.text = "Lives: \(lives)"
         
         if lives <= 0 && !gameOver
         {
@@ -472,6 +499,7 @@ class GameScene: SKScene
             
             view?.presentScene(gameOverScene, transition: reveal)
         }
+        catsLabel.text = "Cats: \(trainCount)"
     }
     
     func loseCats()
